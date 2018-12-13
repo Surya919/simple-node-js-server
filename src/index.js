d0692638cf427bed655e6956f6e1eb2fd3aaa19c
Surya919 <suryaminnikanti870@yahoo.com>
2018-12-14 00:00:15 +0530

    static files serve

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import http from 'http'
+import nStatic from 'node-static';
+
+var fileServer = new nStatic.Server('./public');
 
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('Welcome User')
+  fileServer.serve(req, res);
+  //res.writeHead(200, {'Content-Type': 'text/plain'})
+  //res.end('Welcome Users')
 }).listen(1337, '127.0.0.1')
 
 console.log('Sever running at http://127.0.0.1:1337')
